eb5e22ec665bdb09587ce30acc22bd640432aa34
jsc1463 <>
2018-11-08 15:40:31 +0900

    코틀린 기본 프로젝트 생성.

---
+++ app/src/main/java/dagger2/jsc/com/dagger2kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package dagger2.jsc.com.dagger2kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
